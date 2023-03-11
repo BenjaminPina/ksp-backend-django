9847abc77973edc455b2c4217232e2123c357371
Benjamín Piña Altamirano <benjamin.pina@gmail.com>
2023-03-11 17:15:31 -0600

    creados modelos y aplicadas migraciones

--- empleados/models.py
+++ empleados/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Empleado(models.Model):
+    foto = models.ImageField(blank=True, null=True, upload_to='fotos/')
+    nombre_completo = models.CharField(max_length=255)
+    puesto_trabajo = models.CharField(
+        max_length=120,
+        verbose_name='Puesto de trabajo',
+    )
+    salario = models.FloatField(default=0.0)
+    estatus = models.BooleanField(default=True)
+    fecha_contratación = models.DateField(verbose_name='Fecha de contratación')
+    creado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now=True)
+
+
+class Beneficiario(models.Model):
+    SEXOS = (
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+    )
+    empleado = models.OneToOneField(
+        Empleado,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    nombre_completo = models.CharField(max_length=255, blank=True)
+    parentesco = models.CharField(max_length=120, blank=True)
+    fecha_nacimiento = models.DateField(
+        verbose_name='Fecha de nacimiento',
+        blank=True
+    )
+    sexo = models.CharField(max_length=1, choices=SEXOS, default='M')
+    creado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now=True)
