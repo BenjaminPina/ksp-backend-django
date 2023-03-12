c28dcc88b8a7c0f070658e174e7ae569ad3b0f06
Benjamín Piña Altamirano <benjamin.pina@gmail.com>
2023-03-11 18:24:33 -0600

    CRUD de empleados completado

--- ksp/urls.py
+++ ksp/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from empleados import urls as empleados_urls
 
@@ -8,3 +10,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/', include(empleados_urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
